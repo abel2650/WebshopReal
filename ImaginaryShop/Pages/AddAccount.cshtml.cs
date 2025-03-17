f2370644281b2032ef3cac3830473392472cf686
Mathias <mathiasabelandersen@gmail.com>
2025-03-17 11:41:04 +0100

    opdateret add

--- ImaginaryShop/Pages/AddAccount.cshtml.cs
+++ ImaginaryShop/Pages/AddAccount.cshtml.cs
@@ -1,3 +1,5 @@
+using ImaginaryShop.Model;
+using ImaginaryShop.Model.Repos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,29 @@ namespace ImaginaryShop.Pages
 {
     public class AddAccountModel : PageModel
     {
+        private readonly IUserRepository _userRepository;
+
+        [BindProperty]
+        public User NewUser { get; set; }
+
+        public AddAccountModel(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _userRepository.CreateUser(NewUser);
+            return RedirectToPage("Index");
         }
     }
 }
