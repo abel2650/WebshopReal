dc4f3fe3557fe525276909ccb78dad9e96c13ea7
Mathias <mathiasabelandersen@gmail.com>
2025-03-17 10:25:25 +0100

    Oprettet søge funktion.

--- ImaginaryShop/Pages/Products.cshtml.cs
+++ ImaginaryShop/Pages/Products.cshtml.cs
@@ -1,28 +1,34 @@
 using ImaginaryShop.Model;
 using ImaginaryShop.Model.Repos;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ImaginaryShop.Pages
 {
     public class ProductsModel : PageModel
     {
-        IEnumerable<Product> products;
+        private IEnumerable<Product> products;
+        public IEnumerable<Product> Products { get; private set; }
+        public string SearchTerm { get; set; } // Tilføjet søgeterm
 
-        public IEnumerable<Product> Products { get => products; set => products = value; }
-
-        public void OnGet()
+        public void OnGet(string searchTerm)
         {
+            SearchTerm = searchTerm;
             ProductRepository r = new ProductRepository("Data Source=mssql16.unoeuro.com,1433;Initial Catalog=mathiasabel_dk_db_abel;User ID=mathiasabel_dk;Password=Hnmxry4ftGBFzeadDwgp;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
+            string currency = Request.Cookies.TryGetValue("currency", out string currencyValue) ? currencyValue : null;
+            products = r.GetAllProducts(currency);
 
-            //Her aflæser vi lige om currency cookie er sat
-            string currency = null;
-            if (Request.Cookies.TryGetValue("currency", out string currencyValue))
+            // Filtrér produkterne baseret på søgning
+            if (!string.IsNullOrEmpty(SearchTerm))
             {
-                currency = currencyValue;
+                Products = products.Where(p => p.ProductName.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase));
             }
-            Products = r.GetAllProducts(currency);
+            else
+            {
+                Products = products;
+            }
         }
     }
 }
