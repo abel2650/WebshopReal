86edc8be47109b353b1f0e2d8a80004127807a10
Mathias <mathiasabelandersen@gmail.com>
2025-03-28 10:24:48 +0100

    lavet user story 5

--- ImaginaryShop/Pages/ShoppingBasket.cshtml.cs
+++ ImaginaryShop/Pages/ShoppingBasket.cshtml.cs
@@ -2,52 +2,61 @@ using ImaginaryShop.Model;
 using ImaginaryShop.Model.Repos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Diagnostics;
 using System.Text.Json;
 
 namespace ImaginaryShop.Pages
 {
     public class ShoppingBasketModel : ShoppingPageModel
     {
+        public ShoppingBasket Basket { get; set; } = new();
+
         public IActionResult OnGet()
         {
-            ShoppingBasket basket = GetShoppingBasket();
-            return new JsonResult(new
+            Basket = GetShoppingBasket();
+            return Page();
+        }
+
+        public IActionResult OnPostUpdateQuantity(int productId, string action)
+        {
+            Basket = GetShoppingBasket();
+            var item = Basket.Products.FirstOrDefault(p => p.Product.ProductID == productId);
+            
+            if (item != null)
             {
-                total = basket.GetTotal(),
-                itemCount = basket.GetQuantity()
-            });
-
+                if (action == "increase")
+                {
+                    item.Quantity++;
+                }
+                else if (action == "decrease" && item.Quantity > 1)
+                {
+                    item.Quantity--;
+                }
+                SaveBasket(Basket);
+            }
+            
+            return new JsonResult(new { success = true });
         }
 
-        public void OnPost()
+        public IActionResult OnPostRemoveItem(int productId)
         {
-
-
-      
+            Basket = GetShoppingBasket();
+            var item = Basket.Products.FirstOrDefault(p => p.Product.ProductID == productId);
+            
+            if (item != null)
+            {
+                Basket.Products.Remove(item);
+                SaveBasket(Basket);
+            }
+            
+            return new JsonResult(new { success = true });
         }
 
-
-        public void OnPostAddToCart(int productId)
-        {
-
-
-        }
-
-        /// <summary>
-        /// Føjer et produkt til brugerens indkøbskurv med et item.
-        /// </summary>
-        /// <param name="productId">ID på det produkt, der skal tilføjes til kurven.</param>
-        /// <returns>
-        /// Returnerer en JSON-respons med den opdaterede totalpris og antal varer i kurven.
-        /// Hvis brugeren er administrator, returneres en 403 Forbidden.
-        /// </returns>
         public IActionResult OnPostQuickAdd(int productId)
         {
-            // Henter brugerens indkøbskurv fra sessionen
-            ShoppingBasket basket = GetShoppingBasket();
+            // Henter brugerens indkÃ¸bskurv fra sessionen
+            Basket = GetShoppingBasket();
 
-            // Hvis brugeren er administrator, forbydes adgang til at tilføje produkter
+            // Hvis brugeren er administrator, forbydes adgang til at tilfÃ¸je produkter
             if (User.IsInRole("Admin"))
             {
                 return Forbid(); // Returnerer en HTTP 403 Forbidden
@@ -56,25 +65,24 @@ namespace ImaginaryShop.Pages
             // Opretter et repository-objekt for at hente produktdata fra databasen
             ProductRepository r = new ProductRepository("Data Source=mssql16.unoeuro.com,1433;Initial Catalog=mathiasabel_dk_db_abel;User ID=mathiasabel_dk;Password=Hnmxry4ftGBFzeadDwgp;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
-            // TODO: Implementer tjek af lagerbeholdning, før produktet tilføjes
+            // TODO: Implementer tjek af lagerbeholdning, fÃ¸r produktet tilfÃ¸jes
             Product productToBeAdded = r.GetProductById(productId, Currency);
 
             if (productToBeAdded != null)
             {
-                // Produktet findes i databasen og tilføjes til indkøbskurven med en mængde på 1
-                basket.AddProduct(productToBeAdded, 1);
+                // Produktet findes i databasen og tilfÃ¸jes til indkÃ¸bskurven med en mÃ¦ngde pÃ¥ 1
+                Basket.AddProduct(productToBeAdded, 1);
             }
 
-            // Gemmer den opdaterede indkøbskurv i sessionen
-            SaveBasket(basket);
+            // Gemmer den opdaterede indkÃ¸bskurv i sessionen
+            SaveBasket(Basket);
 
             // Returnerer en JSON-respons med den opdaterede totalpris og antal varer i kurven
             return new JsonResult(new
             {
-                total = basket.GetTotal(),
-                itemCount = basket.GetQuantity()
+                total = Basket.GetTotal(),
+                itemCount = Basket.GetQuantity()
             });
         }
-
     }
 }
